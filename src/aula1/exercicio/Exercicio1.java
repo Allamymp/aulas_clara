b931e3c1097ce81c160916ee9a7e7065b49a30bc
Allamymp <allamympereira@gmail.com>
2024-07-15 17:32:45 -0300

    primeira atividade adicionada

---
+++ src/aula1/exercicio/Exercicio1.java
@@ -0,0 +1,10 @@
+package aula1.exercicio;
+
+public class Exercicio1 {
+
+    public static void main(String[] args){
+
+        // crie abaixo as funcoes e variaveis necessarias para calcular se alguem passou de ano. Considere a nota 7
+        // como media.
+    }
+}
