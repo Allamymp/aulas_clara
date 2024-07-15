2b71f00a49179c17d2fae0c5e9277d4fd6a25fd3
Allamy Monteiro <Allamympereira@gmail.com>
2024-07-15 17:33:37 -0300

    Update Exercicio1.java

--- src/aula1/exercicio/Exercicio1.java
+++ src/aula1/exercicio/Exercicio1.java
@@ -6,5 +6,6 @@ public class Exercicio1 {
 
         // crie abaixo as funcoes e variaveis necessarias para calcular se alguem passou de ano. Considere a nota 7
         // como media.
+        //alterei.
     }
 }
